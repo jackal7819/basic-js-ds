de2a0c477aef148618a86534713f5584e761fb30
jackal7819 <jackal3166@gmail.com>
2024-12-20 22:46:59 +0200

    feat: pass first 4 tests out of 20

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor() {
+		this.stack = [];
+	}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	push(value) {
+		this.stack.push(value);
+	}
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	peek() {
+		return this.stack[this.stack.length - 1];
+	}
+
+	pop() {
+		return this.stack.pop();
+	}
 }
 
 module.exports = {
-  Stack
+	Stack,
 };
